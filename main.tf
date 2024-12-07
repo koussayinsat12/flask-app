edde65ad405d2c391a895ec2b3f846aee8292a11
GHAOUARI KOUSAI <91798061+koussayinsat12@users.noreply.github.com>
2024-12-07 08:31:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # Provider configuration
 provider "azurerm" {
   features {}
+
   client_id       = var.client_id
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
@@ -12,8 +13,8 @@ data "azurerm_resource_group" "existing" {
   name = "devops"
 }
 
-# Azure App Service Plan
-resource "azurerm_app_service_plan" "app_service_plan" {
+# Azure Service Plan (Replacing deprecated resource)
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "flask-app-service-plan"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
@@ -31,7 +32,7 @@ resource "azurerm_app_service" "flask_app_service" {
   name                = "flask-app-service"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
 
   site_config {
     app_command_line = "gunicorn --bind 0.0.0.0:8000 app:app" # Update based on app requirements
@@ -40,12 +41,16 @@ resource "azurerm_app_service" "flask_app_service" {
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
+
+  # Optional explicit dependency
+  depends_on = [azurerm_service_plan.app_service_plan]
 }
 
-# Deployment from GitHub
+# Deployment from GitHub (Removing `manual_integration`)
 resource "azurerm_app_service_source_control" "flask_app_source_control" {
-  app_id             = azurerm_app_service.flask_app_service.id
-  branch             = "main" # Update as necessary
-  repo_url           = "https://github.com/koussayinsat12/flask-app.git"
-  manual_integration = true
+  app_id   = azurerm_app_service.flask_app_service.id
+  branch   = "main" # Update branch if necessary
+  repo_url = "https://github.com/koussayinsat12/flask-app.git"
+
+  # No manual_integration; automatic integration is used by default
 }
