f914a2c29818a9c9c3f6939d15692a5f5812538e
koussayinsat12 <kousai.ghaouari@insat.ucar.tn>
2024-12-08 15:05:31 +0100

    hhh

--- main.tf
+++ main.tf
@@ -42,10 +42,8 @@ resource "azurerm_linux_web_app" "example" {
   service_plan_id     = azurerm_service_plan.example.id
 
   site_config{
-
     always_on     = false
     http2_enabled = true
-
   }
 
   application_stack{
