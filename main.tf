8c91c786cc4832d2026a80f7c49dad26f41caf66
koussayinsat12 <kousai.ghaouari@insat.ucar.tn>
2024-12-08 10:54:10 +0100

    fixed

--- main.tf
+++ main.tf
@@ -25,38 +25,64 @@ resource "azurerm_service_plan" "app_service_plan" {
   name                = "flask-app-service-plan"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  os_type             = "Linux" 
-  sku_name            = "B1"    
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 # Azure Linux Web App for Flask App
 resource "azurerm_linux_web_app" "flask_app_service" {
-  name                = "flask-app-service-${random_string.suffix.result}" # Dynamic name
+  name                = "flask-app-service-${random_string.suffix.result}"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
-    app_command_line = "gunicorn --bind 0.0.0.0:8000 src.app:app"
+    app_command_line = "gunicorn --bind 0.0.0.0:8000 app:app"
   }
 
   app_settings = {
-  "WEBSITE_RUN_FROM_PACKAGE" = "1"
-  "PYTHON_VERSION"           = "3.9" 
-}
-
+    "WEBSITE_RUN_FROM_PACKAGE"            = "1"
+    "PYTHON_VERSION"                      = "3.9"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"      = azurerm_application_insights.app_insights.instrumentation_key
+  }
 }
 
 # Deployment from GitHub
 resource "azurerm_app_service_source_control" "flask_app_source_control" {
-  app_id   = azurerm_linux_web_app.flask_app_service.id
-  branch   = "main" 
-  repo_url = "https://github.com/koussayinsat12/flask-app.git"
+  app_id                = azurerm_linux_web_app.flask_app_service.id
+  branch                = "main"
+  repo_url              = "https://github.com/koussayinsat12/flask-app.git"
   use_manual_integration = true
 }
 
+# GitHub Token for Deployment
 resource "azurerm_source_control_token" "source_control_token" {
   type         = "GitHub"
   token        = var.github_auth_token
   token_secret = var.github_auth_token
+}
+
+# Monitoring: Application Insights
+resource "azurerm_application_insights" "app_insights" {
+  name                = "flask-app-insights-${random_string.suffix.result}"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+  application_type    = "web"
+}
+
+# Monitoring: Diagnostic Settings
+resource "azurerm_monitor_diagnostic_setting" "app_service_diagnostics" {
+  name                       = "flask-app-diagnostics"
+  target_resource_id         = azurerm_linux_web_app.flask_app_service.id
+  log_analytics_workspace_id = azurerm_application_insights.app_insights.id
+
+  log {
+    category = "AppServiceHTTPLogs"
+    enabled  = true
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
 }
