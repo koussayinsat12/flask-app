8f4b6c6dec05ca3636d8e62c1489253406cd81b4
koussayinsat12 <kousai.ghaouari@insat.ucar.tn>
2024-12-08 13:22:07 +0100

    ff

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ data "azurerm_resource_group" "existing" {
   name = "devops"
 }
 
+# Check if the Service Plan already exists
+data "azurerm_service_plan" "existing" {
+  name                = "flask-app-service-plan"
+  resource_group_name = data.azurerm_resource_group.existing.name
+}
+
 # Generate a unique suffix for resource names
 resource "random_string" "suffix" {
   length  = 6
@@ -22,6 +28,7 @@ resource "random_string" "suffix" {
 
 # Azure Service Plan
 resource "azurerm_service_plan" "example" {
+  count               = length(data.azurerm_service_plan.existing.id) == 0 ? 1 : 0
   name                = "flask-app-service-plan"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
@@ -42,7 +49,10 @@ resource "azurerm_linux_web_app" "example" {
   name                = "web-app-${random_string.suffix.result}" # Ensure a globally unique name
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  service_plan_id     = azurerm_service_plan.example.id
+  service_plan_id     = coalesce(
+    try(azurerm_service_plan.example[0].id, null),
+    data.azurerm_service_plan.existing.id
+  )
 
   site_config {
     always_on     = false
@@ -50,7 +60,7 @@ resource "azurerm_linux_web_app" "example" {
   }
 
   app_settings = {
-    "DOCKER_ENABLE_CI"        = "true",
+    "DOCKER_ENABLE_CI"        = "true"
     "DOCKER_CUSTOM_IMAGE_NAME" = var.docker_image
   }
 
