7b768047b5b6f411610a790339cdd9108d71bce8
GHAOUARI KOUSAI <91798061+koussayinsat12@users.noreply.github.com>
2024-12-10 15:29:19 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,8 +3,6 @@ variable "client_secret" {}
 variable "tenant_id" {}
 variable "subscription_id" {}
 variable "docker_image" {
-    default = "nginx:latest"
 }
 variable "location" {
-  default = "France Central" 
 }
